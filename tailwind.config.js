b6118714bba5948020091e1290fa8ed20e1d492b
kxng0109 <iluhjosh13@gmail.com>
2022-05-20 02:31:16 +0100

    Started working on some stylings.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
 	content: ["./public/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				"grey-shade" : "#4c4d5f",
+				"green-shade" : "#00b289",
+				"yellow-shade" : "#f8db62"
+			},
+			zIndex:{
+				"1" : "1",
+				"2" : "2",
+				"3" : "3",
+				"4" : "4"
+			},
+		},
 	},
 	plugins: [],
 }
