843755bd18065f9e7328a6eda4b56ea33ced0a81
kxng0109 <iluhjosh13@gmail.com>
2022-05-23 19:36:48 +0100

    Changed some javascript.

--- public/index.js
+++ public/index.js
@@ -3,6 +3,10 @@ const faqQuestionsDiv = document.querySelectorAll('.faqs--questions-div');
 const footerSubTemplatesName = document.querySelectorAll('.footer-sub-templates--name');
 const footerSubTemplatesDropdown = document.querySelectorAll('.footer-sub-templates--dropdown');
 const getStyleValue = (element, property) => parseInt(getComputedStyle(element, undefined).getPropertyValue(property));
+const nodeListArray = Array.from(footerSubTemplatesDropdown); //Array.from() converts the nodelist into an array
+let initialHeight = nodeListArray.map(data =>{
+	return getStyleValue(data, 'height')
+})
 
 faqQuestionsDiv.forEach((element, index) =>{
 	faqQuestionsDiv[index].onclick = () =>{
@@ -11,7 +15,9 @@ faqQuestionsDiv.forEach((element, index) =>{
 })
 
 footerSubTemplatesName.forEach((element, index) =>{
+	nodeListArray[index].style.height = '0'
+	console.log(`${initialHeight[index]}px`)
 	footerSubTemplatesName[index].onclick = () =>{
-		footerSubTemplatesDropdown[index].style.height = !getStyleValue(footerSubTemplatesDropdown[index], 'height') ? 'auto' : '0px';
+		footerSubTemplatesDropdown[index].style.height = !getStyleValue(footerSubTemplatesDropdown[index], 'height') ? `${initialHeight[index]}px` : '0';
 	}
 })
